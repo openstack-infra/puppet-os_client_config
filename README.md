f0f32ffc9912125c13a10f1203d4c2f3304683a2
Spencer Krum <nibz@spencerkrum.com>
2015-07-08 11:10:53 -0700

    Manage OSCC config file with Puppet
    Takes the approach of passing around data structures.
    
    Change-Id: I0a1f92070bd8ef1933d94c8899b4e1ff9b3bb496

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # OpenStack os_client_config Module
 
 This module installs and configures os_client_config config files.
+
+This module takes hashes as parameters to configure oscc. These can be looked up in hiera directly
+or merged or anything you wish.
+
+When run as root, the puppet module puts the config into the SITE_CONFIG_DIR. When run as notroot
+it puts the config into the USER_CONFIG_DIR.
+
+
+```puppet
+class { 'os_client_config':
+  cache => {
+    'class' => 'dogpile.cache.pylibmc',
+    'max_age' => 3600,
+    'arguments' => {
+      'url' => [
+        '127.0.0.1',
+      ],
+    }
+  },
+  clouds => {
+   'mordred' => {
+     'profile' => 'hp',
+     'auth' => {
+       'username' => 'mordred@inaugust.com',
+       'password' => 'XXXXXXXXX',
+       'project_name' => 'mordred@inaugust.com',
+     },
+     'region_name' => 'region-b.geo-1',
+     'dns_service_type' => 'hpext:dns',
+     'compute_api_version' => '1.1',
+   },
+   'monty' => {
+     'auth' => {
+       'auth_url' => 'https://region-b.geo-1.identity.hpcloudsvc.com:35357/v2.0',
+       'username' => 'monty.taylor@hp.com',
+       'password' => 'XXXXXXXX',
+       'project_name' => 'monty.taylor@hp.com-default-tenant',
+     },
+     'region_name' => 'region-b.geo-1',
+     'dns_service_type' =>  'hpext:dns',
+   },
+   'infra' => {
+     'profile' => 'rackspace',
+     'auth' => {
+       'username' => 'openstackci',
+       'password' => 'XXXXXXXX',
+       'project_id' => '610275',
+     },
+     'region_name' => 'DFW,ORD,IAD',
+   }
+ },
+}
+```
---
+++ manifests/init.pp
@@ -0,0 +1,92 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# os-client-config puppet module
+# sets up clouds.yaml
+
+# parameters
+# clouds, client, and cache are hashes. See readme for advanced usage.
+#
+# target_user
+# Sets which user to create configuration for. Defaults to 'root' which
+# creates site wide configuraton in $SITE_CONFIG_DIR
+
+class os_client_config (
+  $clouds,
+  $config_mode = '0600',
+  $target_user = 'root',
+  $config_group = $::id,
+  $client = {},
+  $cache = {},
+){
+
+  validate_hash($clouds)
+  validate_hash($client)
+  validate_hash($cache)
+
+  $config = {
+    'clouds' => $clouds,
+    'cache'  => $cache,
+    'client' => $client,
+  }
+
+  $user_config_dir = $::kernel ? {
+    'Linux'   => "/home/${::id}/.config/openstack",
+    'Windows' => "C:\\Users\\${::id}\\AppData\\Local\\OpenStack\\openstack",
+    'Darwin'  => "/home/${::id}/Library/Application Support/openstack",
+  }
+
+  $site_config_dir = $::kernel ? {
+    'Linux'   => '/etc/openstack',
+    'Windows' => 'C:\ProgramData\OpenStack\openstack',
+    'Darwin'  => '/Library/Application Support/openstack',
+  }
+
+  $ds = $::kernel ? {
+    'Linux'   => '/',
+    'Windows' => '\\',
+    'Darwin'  => '/',
+  }
+
+  if $target_user != 'root' {
+    # This is, suprisingly, windows-safe
+    $config_path = "${user_config_dir}${ds}clouds.yaml"
+    if !defined(File[$user_config_dir]) {
+      file { $user_config_dir:
+        ensure => directory,
+        owner  => $target_user,
+        group  => $target_user,
+      }
+    }
+  } else {
+    # This is, suprisingly, windows-safe
+    $config_path = "${site_config_dir}${ds}clouds.yaml"
+    if !defined(File[$site_config_dir]) {
+      file { $site_config_dir:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+      }
+    }
+  }
+
+  file { $config_path:
+    ensure  => file,
+    owner   => $target_user,
+    mode    => $config_mode,
+    group   => $config_group,
+    content => template('os_client_config/clouds.yaml.erb'),
+  }
+
+}
